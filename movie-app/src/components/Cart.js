28bf92387dbaade50f99b612279f257bf85ed9b6
Ujjwal3009 <Ujjwal3009.iimt@gmail.com>
2022-11-27 21:18:21 +0530

    Counter Functionality added

---
+++ movie-app/src/components/Cart.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+
+function Cart(){
+    const [count,setCount] = useState(0);
+    function increment(){
+        setCount(count+1);
+    }
+
+    function decrement(){
+        setCount(count-1);
+    }
+    return(
+        <div>
+               <button onClick={increment}>increment</button>
+               <h1>{count}</h1>
+               <button onClick={decrement}>decrement</button>
+
+
+
+        </div>
+      
+    )
+}
+
+export default Cart;
